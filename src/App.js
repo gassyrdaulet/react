b44a201cc0eb1e547b4dd7100b1583f0476cb3c5
Gassyr <gassyrdaulet@gmail.com>
2022-09-17 18:20:20 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [info, setInfo] = useState(0)
+
+  const fetchData = async () => {
+    setInfo((await axios.get('http://localhost:8080/data')).data)
+  }
+
+  useEffect( () => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className='App-logo'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+        {
+          info===0?
+            ''
+          :
+            info.map((info) => <p>{info}</p>)
+        }
     </div>
   );
 }
